f7a4a184d02f3f5d17cccab4b266e42cded381ee
leechongyan <leechongyan33@gmail.com>
2019-03-06 02:14:50 +0800

    userinputvalidation

--- app/src/main/java/com/example/irecycle/Registration.java
+++ app/src/main/java/com/example/irecycle/Registration.java
@@ -9,6 +9,9 @@ import android.content.Intent;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 //https://www.youtube.com/watch?v=1WPAXHhG6u0
 //C:\Users\leech\AppData\Local\Android\Sdk\extras\intel\Hardware_Accelerated_Execution_Manager
 public class Registration extends AppCompatActivity {
@@ -46,22 +49,51 @@ public class Registration extends AppCompatActivity {
                if (s1.equals("")||s2.equals("")||s3.equals("")||s4.equals("")||s5==null){
                    Toast.makeText(getApplicationContext(), "Fields are empty", Toast.LENGTH_SHORT).show();
                }else{
-                   int i5=Integer.parseInt(s5);
-                   if(s2.equals(s3)){
-                        User user = new User(s4,s1,s2,i5);
-                        if(db.getUser(s4)==null){
-                            db.insert(user);
-                            Toast.makeText(getApplicationContext(), "Registered Successfully", Toast.LENGTH_SHORT).show();
-                            i = new Intent(Registration.this,Login.class);
-                            startActivity(i);
-                        }else{
-                            Toast.makeText(getApplicationContext(), "Email has already existed", Toast.LENGTH_SHORT).show();
-                        }
-                   }else{
-                       Toast.makeText(getApplicationContext(), "Passwords are not the same", Toast.LENGTH_SHORT).show();
+                   if(s1.length()<4||s1.length()>19){
+                       e1.setError("Username must be between 3 to 20 letters long");
+                   }
+                   if(s5.length()!=8){
+                       e5.setError("Phone no is invalid");
+                   }
+                   if(s2.length()<8||!isValidPassword(s2)){
+                       e2.setError("Password must contain minimum 8 characters at least 1 Alphabet, 1 Number and 1 Special Character");
+                   }
+                   if(!isEmailValid(s4)){
+                       e4.setError("Email is invalid");
+                   }else {
+                       int i5 = Integer.parseInt(s5);
+                       if (s2.equals(s3)) {
+                           User user = new User(s4, s1, s2, i5);
+                           if (db.getUser(s4) == null) {
+                               db.insert(user);
+                               Toast.makeText(getApplicationContext(), "Registered Successfully", Toast.LENGTH_SHORT).show();
+                               i = new Intent(Registration.this, Login.class);
+                               startActivity(i);
+                           } else {
+                               Toast.makeText(getApplicationContext(), "Email has already existed", Toast.LENGTH_SHORT).show();
+                           }
+                       } else {
+                           Toast.makeText(getApplicationContext(), "Passwords are not the same", Toast.LENGTH_SHORT).show();
+                       }
                    }
                }
            }
         });
+    }
+
+    public static boolean isValidPassword(final String password) {
+        Pattern pattern;
+        Matcher matcher;
+        final String PASSWORD_PATTERN = "^(?=.*[0-9])(?=.*[A-Z])(?=.*[@#$%^&+=!])(?=\\S+$).{4,}$";
+        pattern = Pattern.compile(PASSWORD_PATTERN);
+        matcher = pattern.matcher(password);
+        return matcher.matches();
+    }
+
+    public static boolean isEmailValid(String email) {
+        String expression = "^[\\w\\.-]+@([\\w\\-]+\\.)+[A-Z]{2,4}$";
+        Pattern pattern = Pattern.compile(expression, Pattern.CASE_INSENSITIVE);
+        Matcher matcher = pattern.matcher(email);
+        return matcher.matches();
     }
 }
