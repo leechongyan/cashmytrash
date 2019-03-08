27c2db6f7023028523c119e1a3d120a9478acba3
KangYu <b170045@e.ntu.edu.sg>
2019-03-08 22:36:07 +0700

    help la

--- app/src/main/java/com/example/irecycle/HomePage.java
+++ app/src/main/java/com/example/irecycle/HomePage.java
@@ -2,8 +2,13 @@ package com.example.irecycle;
 
 import android.content.Intent;
 import android.content.SharedPreferences;
+import android.support.annotation.NonNull;
+import android.support.design.widget.NavigationView;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
@@ -11,23 +16,62 @@ import android.widget.TextView;
 public class HomePage extends AppCompatActivity {
     SharedPreferences prf;
     Intent i;
+    private DrawerLayout dl;
+    private ActionBarDrawerToggle abdt;
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home_page);
         TextView result = (TextView)findViewById(R.id.resultView);
         Button logout = (Button)findViewById(R.id.logout);
-        i = new Intent(HomePage.this,Login.class);
+
         prf = getSharedPreferences("user_details",MODE_PRIVATE);
         result.setText("Welcome, "+prf.getString("username",null));
+
+        dl = (DrawerLayout) findViewById(R.id.dl);
+        abdt = new ActionBarDrawerToggle(this, dl, R.string.Open, R.string.Close);
+        abdt.setDrawerIndicatorEnabled(true);
+
+        dl.addDrawerListener(abdt);
+        abdt.syncState();
+
+        NavigationView nav_view = (NavigationView) findViewById(R.id.nav_view);
+        nav_view.setNavigationItemSelectedListener(new NavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
+                int id = menuItem.getItemId();
+                switch (id) {
+                    case R.id.user_update:
+                        i = new Intent(HomePage.this, Login.class);
+                        startActivity(i);
+                    default:
+                        return true;
+                }
+
+            }
+        });
+
         logout.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 SharedPreferences.Editor editor = prf.edit();
                 editor.clear();
                 editor.commit();
+                i = new Intent(HomePage.this,Login.class);
                 startActivity(i);
             }
         });
     }
+
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+
+        return abdt.onOptionsItemSelected(item) || super.onOptionsItemSelected(item);
+    }
+
+
+
 }
