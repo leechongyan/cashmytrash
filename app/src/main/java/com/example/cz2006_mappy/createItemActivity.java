458a275bb48b918b7a217dfebbcd5b81f186af51
elbert-widjaja <elbert.bery@gmail.com>
2019-03-15 14:05:19 +0800

    layout fix

--- app/src/main/java/com/example/cz2006_mappy/createItemActivity.java
+++ app/src/main/java/com/example/cz2006_mappy/createItemActivity.java
@@ -1,7 +1,9 @@
 package com.example.cz2006_mappy;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
 
@@ -12,17 +14,26 @@ public class createItemActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_create_item);
 
-        Button createItemActivityButton = (Button) findViewById(R.id.create_item_button);
-        createItemActivityButton.setOnClickListener(new View.OnClickListener() {
+        Button createItemActivityButton = (Button) findViewById(R.id.createItemButton);
+//        createItemActivityButton.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View v) {
+//                //EditText itemName = (EditText)  findViewById(R.id.create_item_name);
+//                //....
+//                //1.validate input
+//                //if validated
+//                //Intent in = new Intent(getApplicationContext(),MainActivity.class);
+//                //startActivity(in);
+//                //else balikin terus kasih error msg popup aja
+//            }
+//        });
+
+        FloatingActionButton createItemBackButton = (FloatingActionButton) findViewById(R.id.createItemBackButton);
+        createItemBackButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                //EditText itemName = (EditText)  findViewById(R.id.create_item_name);
-                //....
-                //1.validate input
-                //if validated
-                //Intent in = new Intent(getApplicationContext(),MainActivity.class);
-                //startActivity(in);
-                //else balikin terus kasih error msg popup aja
+                Intent createItemBackIntent = new Intent(getApplicationContext(), MainActivity.class);
+                startActivity(createItemBackIntent);
             }
         });
     }
